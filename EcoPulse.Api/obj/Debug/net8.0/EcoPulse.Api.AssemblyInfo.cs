349a46aada3df98e1f207934813d9a4669c61d9d
MAR10MM04 <angelmangarrez@gmail.com>
2025-11-17 21:20:46 -0600

    Eliminar archivos de caché generados automáticamente

--- EcoPulse.Api/obj/Debug/net8.0/EcoPulse.Api.AssemblyInfo.cs
+++ EcoPulse.Api/obj/Debug/net8.0/EcoPulse.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EcoPulse.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cfc8fc2af857a1c5f9e1825511357d3f6a197ce4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5f4e375c05e9e5a0fafc945f8274549ea86fe8b")]
 [assembly: System.Reflection.AssemblyProductAttribute("EcoPulse.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EcoPulse.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
